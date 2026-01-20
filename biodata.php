e4a606a746b0d914d74fab04b0858a64ec670387
pratapph <pratapphaug@gmail.com>
2026-01-20 18:55:15 +0800

    first commit

---
+++ biodata.php
@@ -0,0 +1,4 @@
+<?php
+$nama = "PRATAPPH A/L PARAMJOTHI";
+$nomatrik = "18DIT24F1146";
+$kelas = "DIT4E";
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil Saya</title>
+</head>
+<body>
+    <h1>Profil Diriv</h1>
+    
+    <p>Nama: <?= $nama ?></p>
+    <p>No. Matrik: <?= $nomatrik ?></p>
+    <p>Kelas: <?= $kelas ?></p>
+    <p>Asal: Selangor</p>
+
+
+    <img src="profile.jpg" alt="Foto Saya" width="200">
+</body>
+</html>
